68a766c7f0f6ad974e9a993ef274dc22b73cbd7c
kendalwon <kendalwon@gmail.com>
2020-03-02 21:10:08 -0500

    file cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import NotesContainer from './NotesContainer';
 
 ReactDOM.render(<NotesContainer />, document.getElementById('root'));
